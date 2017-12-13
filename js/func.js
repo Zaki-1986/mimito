a2f3fa2982736f93352ab7b1f3b86f73d1e5ef8b
zaki <290437171@qq.com>
2017-12-13 10:45:56 +0800

    init

---
+++ js/func.js
@@ -0,0 +1,20 @@
+﻿function $(id){return document.getElementById(id);}
+function show(o){document.getElementById(o).style.display="block";}
+function hide(o){document.getElementById(o).style.display="none";}
+function geturl(url,id){
+var http=false;
+$(id).innerHTML='<span class="loading">&nbsp;&nbsp;</span>';
+if(window.XMLHttpRequest){http=new XMLHttpRequest();if(http.overrideMimeType){http.overrideMimeType('text/plain');}}else if(window.ActiveXObject){try{http=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{http=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){}}}
+if(!http){alert('Cannot send an XMLHTTP request');return false;}
+http.onreadystatechange=function(){if(http.readyState==4){$(id).innerHTML=http.responseText;}}
+http.open("get", url, true);
+http.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+http.send(null);
+}
+
+
+
+
+
+
+
